9c2da02dbe02f4eebdfd28040e155a7f32c6e65d
Mauricio Giraldo <mgiraldo@gmail.com>
2015-08-17 17:40:42 -0400

    Fixed test due to scope

--- tests/unit/applicationTest.php
+++ tests/unit/applicationTest.php
@@ -32,10 +32,10 @@ class applicationTest extends PHPUnit_Framework_TestCase {
         $app = new application();
         $app->load();
         $app->process("Index","test");
-        $this->assertTrue(is_a($app->controller,"Index"));
+        $this->assertTrue(is_a($app->controller,"index"));
         $this->assertEquals($app->controller->test(),"foo");
         $this->assertEquals($app->controller->table,"foo");
-        $this->assertEquals($app->render("Index","test"),"Some text inside the view.");
+        $this->assertEquals($app->render("index","test"),"Some text inside the view.");
     }
 
     public function testApplicationIndependently() {
